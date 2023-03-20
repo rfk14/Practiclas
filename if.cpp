b90b2c4c3b0bd2efaa92be8adda562d5a1f75e1c
Farhan Ali Khan <113326511+rfk14@users.noreply.github.com>
2023-03-20 20:05:54 +0500

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,12 @@
+// Practical 01 || SLO 12.1.2
+#include <iostream>
+using namespace std;
+
+int main(){
+	int age;
+	cout << "Enter Your Age in Years: ";
+	cin >> age;
+	if(age >= 18){
+		cout << "Your Are Eligible To Apply For CNIC.";
+	}
+}
